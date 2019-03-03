8d349f3dc9a434448abbaf3cc3a908b317b01bba
dragon <809629457@qq.com>
2019-03-04 07:45:59 +0800

    Initial commit

---
+++ lib/my.dart
@@ -0,0 +1,42 @@
+void main(){
+    int i = 10;
+//    print(i);
+
+    List<int> n = new List();
+    n.add(1);
+    n.add(100);
+    n.add(19);
+    print(n[1]);
+
+    //冒泡排序
+    List<int> m = [5,78,65,1,4,35,76,0,16];
+    int temp;
+    for(int i = 0; i < m.length - 1; i++){
+        for(int j = 0; j < m.length - 1 - i; j++){
+            if(m[j] > m[j+1]){
+                temp = m[j];
+                m[j] = m[j+1];
+                m[j+1] = temp;
+            }
+        }
+    }
+    print(m);
+    //选择排序
+    List<int> arr = [5,78,65,1,4,35,76,0,16];
+    for (int i = 0; i < arr.length - 1; i++) {
+        int minIndex = i; // 用来记录最小值的索引位置，默认值为i
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j; // 遍历 i+1~length 的值，找到其中最小值的位置
+            }
+        }
+        // 交换当前索引 i 和最小值索引 minIndex 两处的值
+        if (i != minIndex) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        // 执行完一次循环，当前索引 i 处的值为最小值，直到循环结束即可完成排序
+    }
+    print(arr);
+}
